54ab8dcf9a9e9ebc90de7d2a6cbb897c07064e00
Frimas9119 <90787948+Frimas9119@users.noreply.github.com>
2022-05-25 22:58:57 +0300

    Add files via upload

---
+++ 8.2/8.2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int SerchB(const string s)
+{
+	int k = 0;
+	int y= s.length();
+	for (int i = 0;i < y;i++) {
+		if (s[i] == 'B' || (s[i] == ' ' && s[i + 1] == 'b')) {
+			k++;
+		}
+	}
+	return k;
+}
+
+int main()
+{
+	string str;
+	cout << "Enter string:" << endl;
+	getline(cin, str);
+	cout << "The number of words that begin with B: " << SerchB(str) << endl;
+	return 0;
+}
